a679d47c4a1ab4c8bc8f3ae61ed7f3c0a4f7ac5c
whejy <john.wheldale@outlook.com>
2022-05-12 14:20:31 +0930

    initial

---
+++ backend/vault/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.core.validators import URLValidator
+
+
+class Vault(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    description = models.CharField(max_length=100, blank=True)
+    url = models.URLField(max_length=255, blank=True,
+                          validators=[URLValidator])
+
+    def _str_(self):
+        return self.username

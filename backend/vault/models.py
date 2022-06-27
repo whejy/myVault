6f2a461d23176438efc9cd20bc71bd0baffa227b
whejy <john.wheldale@outlook.com>
2022-06-27 16:30:19 +0930

    altered char limits in Vault model field

--- backend/vault/models.py
+++ backend/vault/models.py
@@ -7,8 +7,8 @@ from django.forms import ValidationError
 
 class Vault(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
     description = models.CharField(max_length=100, blank=True)
     url = models.URLField(max_length=255, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
